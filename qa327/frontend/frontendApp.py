1d0ba3464dcf72712f4d6a5ceb17d9e8d7f4e8a5
group72 <group72@elec377.queensu.ca>
2020-11-07 01:33:14 -0500

    fixed main

--- qa327/frontend/frontendApp.py
+++ qa327/frontend/frontendApp.py
@@ -6,11 +6,11 @@ import helpers
 def main():
 
     # location of the frontend hosting
-    location = sys.argv[0]
+    location = sys.argv[1]
 
     # load the account list file and the valid ticket list file at given location
-    helpers.ResourcesHelper.loadUserInfo(sys.argv[1])
-    helpers.ResourcesHelper.loadTicketInfo(sys.argv[2])
+    helpers.ResourcesHelper.loadUserInfo(sys.argv[2])
+    helpers.ResourcesHelper.loadTicketInfo(sys.argv[3])
 
     print('Welcome to SeetGeek!')
     print('author @VeryUsefulGroup')
