ac3f484372c0d837197dbbfe04151cab91cf0d4c
Chengzhen Guo <15cg53@queensu.ca>
2020-11-16 19:21:35 -0500

    Added logout test

---
+++ qa327_test/logout/test.py
@@ -0,0 +1,89 @@
+import tempfile
+from importlib import reload
+import os
+import io
+import sys
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append('qa327/frontend')
+import app
+
+################################################################
+# if want to force printing to console:
+# pritn(something)
+# assert false
+################################################################
+
+def test_r6_1_1(capsys): # Check if the logout command is invalid if user has not logged in
+    helper(capsys=capsys, test_id='r3_1_1')
+
+def test_r6_2_1(capsys): # Check if the user can logout and go back to the landing page
+    helper(capsys=capsys, test_id='r3_2_1')
+
+def helper(
+        capsys,
+        test_id):
+    """ a helper function that test requirements for the example app
+
+    Arguments:
+        capsys -- object created by pytest to capture stdout and stderr
+    """
+
+    # cleanup package
+    reload(app)
+
+    # locate test case folder:
+    case_folder = os.path.join(path, test_id)
+
+    # read terminal input:
+    with open(
+        os.path.join(
+            case_folder, 'terminal_input.txt')) as rf:
+        terminal_input = rf.read().splitlines()
+
+    # read expected tail portion of the terminal output:
+    with open(
+        os.path.join(
+            case_folder, 'terminal_output_tail.txt')) as rf:
+        terminal_output_tail = rf.read().splitlines()
+
+    # create a temporary file in the system to store output transactions
+    temp_fd, temp_file = tempfile.mkstemp()
+    transaction_summary_file = temp_file
+
+    # prepare program parameters
+    sys.argv = [
+        'app.py', 'Kingston',
+        os.path.join(case_folder, 'valid_account_list_file.csv'),
+        os.path.join(case_folder, 'valid_ticket_list_file.csv'),]
+
+    # set terminal input
+    sys.stdin = io.StringIO(
+        '\n'.join(terminal_input))
+
+    # run the program
+    app.main()
+
+    # capture terminal output / errors
+    # assuming that in this case we don't use stderr
+    out, err = capsys.readouterr()
+
+    # split terminal output in lines
+    out_lines = out.splitlines()
+
+    # compare terminal outputs at the end.`
+    for i in range(1, len(terminal_output_tail)+1):
+        index = i * -1
+        assert terminal_output_tail[index] == out_lines[index]
+
+    # compare transactions:
+    with open(transaction_summary_file, 'r') as of:
+        content = of.read()
+        with open(os.path.join(case_folder, 'transaction_summary_file.csv'), 'r') as exp_file_of:
+            expected_content = exp_file_of.read()
+            assert content == expected_content
+
+    # clean up
+    os.close(temp_fd)
+    os.remove(temp_file)
