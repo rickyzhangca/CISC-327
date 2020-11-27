27ed289b5fa0d599df53588c80f92eaf848b62c5
Chengzhen Guo <15cg53@queensu.ca>
2020-11-27 15:46:07 -0500

    Fixing

--- qa327/backend/controller.py
+++ qa327/backend/controller.py
@@ -4,11 +4,13 @@ import services
 class Controller:
 
     def __init__(self, transaction_file):
-        self.userService = services.UserService(transaction_file)
-        self.ticketService = services.TicketService(transaction_file)
+        self.transaction_file = transaction_file
     
     def processOffice(self):
-        self.userService.processTransactions()
-        self.ticketService.processTransactions()
+        self.userService = services.UserService(self.transaction_file)
+        self.userService.processTransactions()        
         self.userService.updateDatabase()
+        
+        self.ticketService = services.TicketService(self.transaction_file)        
+        self.ticketService.processTransactions()
         self.ticketService.updateDatabase()
